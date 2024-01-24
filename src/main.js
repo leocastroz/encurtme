3287056a575f80eeba1800e4dff986fdc2726930
leocastroz <castroz.dev@gmail.com>
2024-01-24 02:31:35 -0300

    feat: notification library and shortener tokens

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
+// STYLE - CSS
 import './assets/main.css'
+import "vue-toastification/dist/index.css";
 
+// LIBS - Vue
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Toast from "vue-toastification";
 
 const app = createApp(App)
 
+// !IMPORTANT - INSTANTIATE LIBS
+app.use(Toast)
 app.use(router)
-
 app.mount('#app')
